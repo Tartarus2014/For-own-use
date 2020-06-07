506dae2abea37f79e90c298c048d14d6606a9919
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2020-06-07 11:55:01 +0800

    Update Zhihu.js

--- Zhihu.js
+++ Zhihu.js
@@ -1,8 +1,8 @@
 # ZhiHu Ads
 
-hostname = www.zhihu.com, api.zhihu.com
+hostname = api.zhihu.com, www.zhihu.com
 
-# 知乎 （www.zhihu.com, api.zhihu.com）
+# 知乎 （api.zhihu.com, www.zhihu.com）
 http-response https://api.zhihu.com/moments/recommend script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20feed.js
 http-response https://api.zhihu.com/topstory/recommend script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20recommend.js
 http-response https://api.zhihu.com/v4/questions script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20answer.js
