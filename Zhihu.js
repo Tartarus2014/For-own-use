1e0ae01db128c0da2f9fbf092bb65d4a13341890
Tartarus <39802246+Tartarus2014@users.noreply.github.com>
2020-06-07 12:00:46 +0800

    Update Zhihu.js

--- Zhihu.js
+++ Zhihu.js
@@ -1,9 +1,10 @@
 # ZhiHu Ads
 
-hostname = api.zhihu.com, www.zhihu.com
+hostname = www.zhihu.com, api.zhihu.com, link.zhihu.com, 118.89.204.198
 
-# 知乎 （api.zhihu.com, www.zhihu.com）
+# 知乎 （www.zhihu.com, api.zhihu.com, link.zhihu.com, 118.89.204.198）
 http-response https://api.zhihu.com/moments/recommend script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20feed.js, requires-body=true, timeout=10, tag=知乎
 http-response https://api.zhihu.com/topstory/recommend script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20recommend.js, requires-body=true, timeout=10, tag=知乎
 http-response https://api.zhihu.com/v4/questions script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20answer.js, requires-body=true, timeout=10, tag=知乎
 http-response https://api.zhihu.com/people/ script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20people.js, requires-body=true, timeout=10, tag=知乎
+http-request https?://link.zhihu.com/\?target= script-path=https://raw.githubusercontent.com/onewayticket255/Surge-Script/master/surge%20zhihu%20link.js, requires-body=true, timeout=10, tag=知乎
